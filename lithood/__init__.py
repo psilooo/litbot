9e1aa4afd36f56e55ce66c8e5fb31e11e436c904
KYL <shadowk@live.ca>
2026-01-19 15:53:30 -0500

    feat: finalize package exports

--- lithood/__init__.py
+++ lithood/__init__.py
@@ -1,3 +1,17 @@
 """LIT Grid Trading Bot for Lighter DEX."""
 
 __version__ = "0.1.0"
+
+from lithood.client import LighterClient
+from lithood.state import StateManager
+from lithood.grid import GridEngine
+from lithood.hedge import HedgeManager
+from lithood.floor import FloorProtection
+
+__all__ = [
+    "LighterClient",
+    "StateManager",
+    "GridEngine",
+    "HedgeManager",
+    "FloorProtection",
+]
